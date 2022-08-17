0acf2f3cba2a713c8811701ecc312949538ff9cf
root <root@DESKTOP-KKQCLCE.localdomain>
2022-08-17 16:47:33 +0530

    Prod Setting File update

--- src/core/settings/prod.py
+++ src/core/settings/prod.py
@@ -2,14 +2,15 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS =['http://zipchodev.com/', 'zipchodev.com'  ,'65.0.89.205','0.0.0.0','http://65.0.89.205/']
-CSRF_TRUSTED_ORIGINS= ['http://zipchodev.com/','zipchodev.com','http://65.0.89.205/','65.0.89.205']
+
+ALLOWED_HOSTS =['3.110.117.161','0.0.0.0','http://3.110.117.161/']
+CSRF_TRUSTED_ORIGINS= ['http://3.110.117.161/','3.110.117.161']
 CORS_ORIGIN_ALLOW_ALL=True
 
 CORS_ORIGIN_WHITELIST=[
     'http://127.0.0.1:9090',
     'http://127.0.0.1:8000',
-    'http://65.0.89.205',
+    'http://3.110.117.161',
     'http://zipchodev.com'
 ]
 
