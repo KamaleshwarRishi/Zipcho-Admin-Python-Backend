3d338da07a842a1d2f920c87e73ddd65a2a1545f
root <root@DESKTOP-KKQCLCE.localdomain>
2022-09-01 17:05:30 +0530

    Test Case Check v1

--- src/authentication/tests.py
+++ src/authentication/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
+
+from authentication.models import User
 
 # Create your tests here.
+class test_newUser(TestCase):
+    def setUp(self) :
+        User.objects.create(email='testuser@gmail.com')
+        
+    def test_email(self):
+        u = User.objects.get(email='testuser@gmail.com')
+        self.assertEqual(u.email, 'testuser@gmail.com')
+    def test_secend(self):
+        pass
